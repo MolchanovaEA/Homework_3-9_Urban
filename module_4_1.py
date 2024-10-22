c83c55d719fcdef505ed7ea81fb6441265364733
MolchanovaEA <molchanowaea@gmail.com>
2024-10-22 23:00:00 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as div_fake
+from true_math import divide as div_true
+print (div_fake(12, 3))
+print (div_fake(12, 0))
+print (div_true(12, 3))
+print (div_true(12, 0))
